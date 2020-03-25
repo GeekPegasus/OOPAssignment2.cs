d9a046d9abc925f43d8d74ee77f020500782aa50
Isobel <bellacarroll97@gmail.com>
2020-03-25 22:11:46 +0000

    Add project files.

---
+++ OOPAssignment2.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace OOPAssignment2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string y = Console.ReadLine();
+            string[] x = y.Split(" ");
+            switch (x[0])
+            {
+                case "diff":
+                    string[] file1 = File.ReadAllLines(x[1]);
+                    string[] file2 = File.ReadAllLines(x[2]);
+                    if (file1.SequenceEqual(file2))
+                    {
+                        Console.WriteLine("The files are the same.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The files are not the same.");
+                    }
+                    break;
+            }
+
+
+        }
+    }
+}
